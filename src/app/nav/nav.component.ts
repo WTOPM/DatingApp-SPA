3778079be628afbde5803199f68c776daecc2408
WTOPM <wtopm@list.ru>
2020-02-13 01:32:02 +0200

    Added login component

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -22,4 +22,13 @@ export class NavComponent implements OnInit {
    });
   }
 
+  loggedIn(){
+    const token = localStorage.getItem('token');
+    return !!token;
+  }
+  logout(){
+    localStorage.removeItem('token');
+    console.log('logged out');
+  }
+
 }
