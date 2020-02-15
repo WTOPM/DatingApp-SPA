10582886331ab4d5239e4d661635f94e4e699846
WTOPM <wtopm@list.ru>
2020-02-15 20:08:44 +0200

    Using JWT library to decode token

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -10,7 +10,7 @@ import { AlertifyService } from '../_services/alertify.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(private authService: AuthService, private alertify: AlertifyService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
