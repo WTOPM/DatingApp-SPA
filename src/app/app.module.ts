99d816d09ce5f3f244810ce1f5925e81585f07fa
WTOPM <wtopm@list.ru>
2020-02-21 11:58:48 +0200

    Implement photo of users on knownAs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,12 +13,13 @@ import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
 import { AlertifyService } from './_services/alertify.service';
-import { MemberListComponent } from './member-list/member-list.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { appRoutes } from './routes';
 import { AuthGuard } from './_guards/auth.guard';
 import { UserService } from './_services/user.service';
+import { MemberCardComponent } from './members/member-list/member-card/member-card.component';
 
 @NgModule({
    declarations: [
@@ -29,7 +30,8 @@ import { UserService } from './_services/user.service';
       MemberListComponent,
       ListsComponent,
       MessagesComponent,
-      MessagesComponent
+      MessagesComponent,
+      MemberCardComponent
    ],
    imports: [
       BrowserModule,
