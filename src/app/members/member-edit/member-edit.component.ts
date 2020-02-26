8a514faea8bb6f0152e73a61fb4969312fe50bed
WTOPM <wtopm@list.ru>
2020-02-26 02:30:43 +0200

    Implement Photo method pn account (v0.12)

--- src/app/members/member-edit/member-edit.component.ts
+++ src/app/members/member-edit/member-edit.component.ts
@@ -14,6 +14,7 @@ import { AuthService } from 'src/app/_services/auth.service';
 export class MemberEditComponent implements OnInit {
   @ViewChild('editForm', {static: true}) editForm: NgForm;
   user: User;
+  photoUrl: string;
   @HostListener('window:beforeunload', ['$event'])
   unloadNotification($event: any) {
     if(this.editForm.dirty) {
@@ -28,6 +29,7 @@ export class MemberEditComponent implements OnInit {
     this.route.data.subscribe(data => {
       this.user = data['user'];
     });
+    this.authService.currentPhotoUrl.subscribe(photoUrl => this.photoUrl = photoUrl);
   }
 
   updateUser() {
@@ -36,8 +38,12 @@ export class MemberEditComponent implements OnInit {
       this.editForm.reset(this.user);
     }, error => {
       this.alertify.error(error);
-    })
+    });
 
+  }
+
+  updateMainPhoto(photoUrl) {
+    this.user.photoUrl = photoUrl;
   }
 
 }
--- src/app/members/photo-editor/photo-editor.component.ts
+++ src/app/members/photo-editor/photo-editor.component.ts
@@ -41,42 +41,42 @@ export class PhotoEditorComponent implements OnInit {
       maxFileSize: 10 * 1024 * 1024
     });
 
-    // this.uploader.onSuccessItem = (item, response, status, headers) => {
-    //   if (response) {
-    //     const res: Photo = JSON.parse(response);
-    //     const photo = {
-    //       id: res.id,
-    //       url: res.url,
-    //       dateAdded: res.dateAdded,
-    //       description: res.description,
-    //       isMain: res.isMain
-    //     };
-    //     this.photos.push(photo);
-    //   }
-    // };
+    this.uploader.onSuccessItem = (item, response, status, headers) => {
+      if (response) {
+        const res: Photo = JSON.parse(response);
+        const photo = {
+          id: res.id,
+          url: res.url,
+          dateAdded: res.dateAdded,
+          description: res.description,
+          isMain: res.isMain
+        };
+        this.photos.push(photo);
+      }
+    };
   }
 
-  // setMainPhoto(photo: Photo) {
-  //   this.userService.setMainPhoto(this.authService.decodedToken.nameid, photo.id).subscribe(() => {
-  //     this.currentMain = this.photos.filter(p => p.isMain === true)[0];
-  //     this.currentMain.isMain = false;
-  //     photo.isMain = true;
-  //     this.authService.changeMemberPhoto(photo.url);
-  //     this.authService.currentUser.photoUrl = photo.url;
-  //     localStorage.setItem('user', JSON.stringify(this.authService.currentUser));
-  //   }, error => {
-  //     this.alertify.error(error);
-  //   });
-  // }
+  setMainPhoto(photo: Photo) {
+    this.userService.setMainPhoto(this.authService.decodedToken.nameid, photo.id).subscribe(() => {
+      this.currentMain = this.photos.filter(p => p.isMain === true)[0];
+      this.currentMain.isMain = false;
+      photo.isMain = true;
+      this.authService.changeMemberPhoto(photo.url);
+      this.authService.currentUser.photoUrl = photo.url;
+      localStorage.setItem('user', JSON.stringify(this.authService.currentUser));
+    }, error => {
+      this.alertify.error(error);
+    });
+  }
 
-  // deletePhoto(id: number) {
-  //   this.alertify.confirm('Are you sure you want to delete this photo?', () => {
-  //     this.userService.deletePhoto(this.authService.decodedToken.nameid, id).subscribe(() => {
-  //       this.photos.splice(this.photos.findIndex(p => p.id === id), 1);
-  //       this.alertify.success('Photo has been deleted');
-  //     }, error => {
-  //       this.alertify.error('Failed to delete the photo');
-  //     });
-  //   });
-  // }
+  deletePhoto(id: number) {
+    this.alertify.confirm('Are you sure you want to delete this photo?', () => {
+      this.userService.deletePhoto(this.authService.decodedToken.nameid, id).subscribe(() => {
+        this.photos.splice(this.photos.findIndex(p => p.id === id), 1);
+        this.alertify.success('Photo has been deleted');
+      }, error => {
+        this.alertify.error('Failed to delete the photo');
+      });
+    });
+  }
 }
