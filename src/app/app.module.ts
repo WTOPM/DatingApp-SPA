0770645dbb55f8eca0200c19d753fbb2053b218d
WTOPM <wtopm@list.ru>
2020-03-03 22:01:40 +0200

    Implement messages on SPA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -35,6 +35,7 @@ import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
 import { FileUploadModule } from 'ng2-file-upload';
 import { ListsResolver } from './_resolvers/lists.resolver';
+import { MessagesResolver } from './_resolvers/messages.resolver';
 
 export function tokenGetter() {
    return localStorage.getItem('token');
@@ -89,7 +90,8 @@ export function tokenGetter() {
       MemberListResolver,
       MemberEditResolver,
       PreventUnsavedChanges,
-      ListsResolver
+      ListsResolver,
+      MessagesResolver
    ],
    bootstrap: [
       AppComponent
--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Message } from '../_models/message';
+import { Pagination, PaginatedResult } from '../_models/pagination';
+import { UserService } from '../_services/user.service';
+import { ActivatedRoute } from '@angular/router';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-messages',
@@ -6,10 +11,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./messages.component.css']
 })
 export class MessagesComponent implements OnInit {
+  messages: Message[];
+  pagination: Pagination;
+  messageContainer: 'Unread';
 
-  constructor() { }
+  constructor(private userService: UserService, private authService,
+              private route: ActivatedRoute, private alertify: AlertifyService) { }
 
   ngOnInit() {
+    this.route.data.subscribe(data => {
+      this.messages = data['messages'].result;
+      this.pagination = data ['messages'].pagination;
+    });
+  }
+
+  loadMessages() {
+    this.userService.getMessages(this.authService.decodedToken.nameid, this.pagination.currentPage,
+      this.pagination.itemsPerPage, this.messageContainer)
+      .subscribe((res: PaginatedResult<Message[]>) => {
+        this.messages = res.result;
+        this.pagination = res.pagination;
+      }, error => {
+        this.alertify.error(error);
+      });
+  }
+
+  pageChanged(event: any): void {
+    this.pagination.currentPage = event.page;
+    this.loadMessages();
   }
 
 }
