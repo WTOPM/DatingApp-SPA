0cc9b48a2f1131d88bd704e76e0f1e932eb26a9f
WTOPM <wtopm@list.ru>
2020-03-04 11:36:54 +0200

    fix

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Message } from '../_models/message';
 import { Pagination, PaginatedResult } from '../_models/pagination';
 import { UserService } from '../_services/user.service';
-import { ActivatedRoute } from '@angular/router';
+import { AuthService } from '../_services/auth.service';
+import { ActivatedRoute } from '../../../node_modules/@angular/router';
 import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
@@ -13,19 +14,20 @@ import { AlertifyService } from '../_services/alertify.service';
 export class MessagesComponent implements OnInit {
   messages: Message[];
   pagination: Pagination;
-  messageContainer: 'Unread';
+  messageContainer = 'Unread';
 
-  constructor(private userService: UserService, private authService,
-              private route: ActivatedRoute, private alertify: AlertifyService) { }
+  constructor(private userService: UserService, private authService: AuthService,
+    private route: ActivatedRoute, private alertify: AlertifyService) { }
 
   ngOnInit() {
     this.route.data.subscribe(data => {
       this.messages = data['messages'].result;
-      this.pagination = data ['messages'].pagination;
+      this.pagination = data['messages'].pagination;
     });
   }
 
   loadMessages() {
+    alert(1);
     this.userService.getMessages(this.authService.decodedToken.nameid, this.pagination.currentPage,
       this.pagination.itemsPerPage, this.messageContainer)
       .subscribe((res: PaginatedResult<Message[]>) => {
@@ -34,6 +36,17 @@ export class MessagesComponent implements OnInit {
       }, error => {
         this.alertify.error(error);
       });
+  }
+
+  deleteMessage(id: number) {
+    this.alertify.confirm('Are you sure you want to delete this message?', () => {
+      this.userService.deleteMessage(id, this.authService.decodedToken.nameid).subscribe(() => {
+        this.messages.splice(this.messages.findIndex(m => m.id === id), 1);
+        this.alertify.success('Message has been deleted');
+      }, error => {
+        this.alertify.error('Failed to delete the message');
+      });
+    });
   }
 
   pageChanged(event: any): void {
